f309063ad70f48efb436e88eca794a8267604a75
SilverFishSeeker <silverfishseeker@gmail.com>
2023-07-01 13:17:17 +0200

    generalización de controladores de modelo (puede que haya erratas)

--- app/controllers/clases_controller.rb
+++ app/controllers/clases_controller.rb
@@ -1,50 +1,11 @@
 class ClasesController < ModelController
 
-  def index
-      @clases = Clase.all
+  def initialize
+    super 
+    @tipo = Clase
   end
 
-  def show
-      @clase = Clase.find(params[:id])
+  def model_params
+    params.require(:clase).permit(:efecto, :descripcion, :nombre, :nivel, :image)
   end
-
-  def new
-    @clase = Clase.new
-  end
-
-  def create
-    @clase = Clase.new(clase_params)
-
-    if @clase.save
-      redirect_to @clase  
-    else
-      redirect_to new_clase_path , notice: 'La jodiste.'
-    end
-  end
-
-  def edit
-    @clase = Clase.find(params[:id])
-  end
-
-  def update
-    @clase = Clase.find(params[:id])
-
-    if @clase.update(clase_params)
-      redirect_to @clase
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @clase = Clase.find(params[:id])
-    @clase.destroy
-
-    redirect_to clases_path, status: :see_other
-  end
-
-  private
-    def clase_params
-      params.require(:clase).permit(:efecto, :descripcion, :nombre, :nivel, :image)
-    end
 end
--- app/controllers/habilidads_controller.rb
+++ app/controllers/habilidads_controller.rb
@@ -1,54 +1,11 @@
 class HabilidadsController < ModelController
-  
-    def index
-        @habilidades = Habilidad.all
-    end
-  
-    def show
-        @habilidad = Habilidad.find(params[:id])
-    end
-  
-    def new
-      @habilidad = Habilidad.new
-    end
-  
-    def create
-      @habilidad = Habilidad.new(habilidad_params)
-  
-      if @habilidad.save
-        redirect_to @habilidad  
-      else
-        redirect_to new_habilidad_path , notice: 'La jodiste.'
-      end
-    end
-  
-    def edit
-      @habilidad = Habilidad.find(params[:id])
-    end
-  
-    def update
-      @habilidad = Habilidad.find(params[:id])
-  
-      if @habilidad.update(habilidad_params)
-        redirect_to @habilidad
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @habilidad = Habilidad.find(params[:id])
-      @habilidad.destroy
-  
-      redirect_to habilidads_path, status: :see_other
-    end
 
-    def habilidadesIndependientes
-      @habilidades = Habilidad.left_outer_joins(:items, :clases).where(items: { id: nil }, clases: { id: nil })
-    end
+  def initialize
+    super 
+    @tipo = Habilidad
+  end
   
-    private
-      def habilidad_params
-        params.require(:habilidad).permit(:nombre, :nivel, :efecto, :image, clase_ids: [], item_ids: [])
-      end
+  def model_params
+    params.require(:habilidad).permit(:nombre, :nivel, :efecto, :image, clase_ids: [], item_ids: [])
+  end
 end
