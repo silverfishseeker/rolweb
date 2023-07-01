154f7fdcaca2c502d62071d8bb71b1123cf5f309
SilverFishSeeker <silverfishseeker@gmail.com>
2023-07-01 13:53:28 +0200

    galería

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,11 @@
 class PicturesController < ModelController
+
+    def initialize
+      super 
+      @tipo = Picture
+    end
+
+    def model_params
+      params.require(:picture).permit(:nombre, :image)
+    end
 end
