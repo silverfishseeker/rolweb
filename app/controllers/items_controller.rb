53741657ffe0e8bf030713a671d6b67671e78909
SilverFishSeeker <silverfishseeker@gmail.com>
2023-07-01 13:06:48 +0200

    generalización de item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,50 +1,11 @@
 class ItemsController < ModelController
-  
-    def index
-        @items = Item.all
+
+    def initialize
+      super 
+      @tipo = Item
     end
-  
-    def show
-        @item = Item.find(params[:id])
+
+    def model_params
+      params.require(:item).permit(:nombre, :coste, :peso, :efecto, :image, clase_ids: [])
     end
-  
-    def new
-      @item = Item.new
-    end
-  
-    def create
-      @item = Item.new(item_params)
-  
-      if @item.save
-        redirect_to @item  
-      else
-        redirect_to new_item_path , notice: 'La jodiste.'
-      end
-    end
-  
-    def edit
-      @item = Item.find(params[:id])
-    end
-  
-    def update
-      @item = Item.find(params[:id])
-  
-      if @item.update(item_params)
-        redirect_to @item
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @item = Item.find(params[:id])
-      @item.destroy
-  
-      redirect_to habilidads_path, status: :see_other
-    end
-  
-    private
-      def item_params
-        params.require(:item).permit(:nombre, :coste, :peso, :efecto, :image, clase_ids: [])
-      end
 end
--- app/controllers/model_controller.rb
+++ app/controllers/model_controller.rb
@@ -1,3 +1,47 @@
 class ModelController < ApplicationController
+    
     http_basic_authenticate_with name: "yomismo", password: "yasabes", except: [:index, :show]
+    
+    def index
+        @xs = @tipo.all
+    end
+  
+    def show
+        @x = @tipo.find(params[:id])
+    end
+  
+    def new
+      @x = @tipo.new
+    end
+  
+    def create
+      @x = @tipo.new(model_params)
+  
+      if @x.save
+        redirect_to @x  
+      else
+        redirect_to "/control" , notice: 'La jodiste.'
+      end
+    end
+  
+    def edit
+      @x = @tipo.find(params[:id])
+    end
+  
+    def update
+      @x = @tipo.find(params[:id])
+  
+      if @x.update(model_params)
+        redirect_to @x
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @x = @tipo.find(params[:id])
+      @x.destroy
+  
+      redirect_to "/control"
+    end
 end
