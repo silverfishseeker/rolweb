3fa81b7506f1f5448b8ca33cac27ff9041343128
SilverFishSeeker <silverfishseeker@gmail.com>
2023-07-04 13:29:42 +0200

    bugs y reglas

--- app/controllers/habilidads_controller.rb
+++ app/controllers/habilidads_controller.rb
@@ -4,6 +4,10 @@ class HabilidadsController < ModelController
     super 
     @tipo = Habilidad
   end
+
+  def habilidadesIndependientes
+    @xs = Habilidad.left_outer_joins(:items, :clases).where(items: { id: nil }, clases: { id: nil })
+  end
   
   def model_params
     params.require(:habilidad).permit(:nombre, :nivel, :efecto, :image, clase_ids: [], item_ids: [])
