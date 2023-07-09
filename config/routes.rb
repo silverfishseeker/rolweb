413b4ce7557933bf5c86b474fc7d456321c85299
SilverFishSeeker <silverfishseeker@gmail.com>
2023-07-09 18:34:44 +0200

    cosillas

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "clases#index"
+  root "info#lore"
   resources :clases
   resources :habilidads
   resources :items
