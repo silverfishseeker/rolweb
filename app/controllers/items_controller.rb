9c32b1c7b95c22dfb317f1ef4863ae1bbd13408d
SilverFishSeeker <silverfishseeker@gmail.com>
2023-11-06 23:53:52 +0100

    mirar items por categorías

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,19 +5,6 @@ class ItemsController < ModelController
     @tipo = Item
   end
 
-  
-  def index
-    if params[:isPeso].present?
-      if params[:isPeso] == "t"
-        @xs = Item.where(peso: 0)
-      elsif params[:isPeso] == "f"
-        @xs = Item.where.not(peso: 0)
-      end
-    else
-      super
-    end
-  end
-
   def model_params
     params.require(:item).permit(:nombre, :coste, :peso, :efecto, :image, categ_ids: [], clase_ids: [])
   end
