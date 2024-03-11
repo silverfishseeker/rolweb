1d54de4c014156afba6d9e1faac55faba243c5f7
SilverFishSeeker <silverfishseeker@gmail.com>
2024-03-11 16:04:09 +0100

    habilidades ocultas

--- app/controllers/habilidads_controller.rb
+++ app/controllers/habilidads_controller.rb
@@ -6,14 +6,16 @@ class HabilidadsController < ModelController
   end
 
   def index
-    if params[:indep] == "t"
+    if params[:mode] == "indep"
       @xs = Habilidad.left_outer_joins(:items, :clases).where(items: { id: nil }, clases: { id: nil })
+    elsif params[:mode] == "hidden"
+      @xs = Habilidad.unscoped.where(oculto: true)
     else
       super
     end
   end
   
   def model_params
-    params.require(:habilidad).permit(:nombre, :nivel, :efecto, :image, clase_ids: [], item_ids: [], categ_ids: [], mob_ids: [])
+    params.require(:habilidad).permit(:nombre, :nivel, :efecto, :image, :oculto, clase_ids: [], item_ids: [], categ_ids: [], mob_ids: [])
   end
 end
--- app/models/habilidad.rb
+++ app/models/habilidad.rb
@@ -4,4 +4,6 @@ class Habilidad < ApplicationRecord
     has_and_belongs_to_many :categs
     has_and_belongs_to_many :mobs
     mount_uploader :image, ImageUploader
+
+    default_scope { where(oculto: false) }
 end
