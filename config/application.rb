d8e1a9fa1e4b458345461f81067b60bbfa6825df
SilverFishSeeker <silverfishseeker@gmail.com>
2024-03-14 12:06:09 +0100

    La fuente. Ostia puta mierda que coñazo ha sido

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Rolweb
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.precompile += %w( *.scss *.sass )
   end
 end
