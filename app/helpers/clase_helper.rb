8e35aad2202082f3ed2c6ce75f59a1e719c92f5a
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2024-05-18 18:16:27 +0200

    terminar el tema de los subtitulos de clases

--- app/helpers/clase_helper.rb
+++ app/helpers/clase_helper.rb
@@ -1,2 +1,11 @@
 module ClaseHelper
+    def clase_subtitulo(clase)
+        raw(clase.radical && clase.raza ?
+            '<h3 class="clase-subtitulo clase-subtitulo_radical">Clase de raza y radical</h3>'
+        : ( clase.radical ?
+            '<h3 class="clase-subtitulo clase-subtitulo_radical"> Clase radical</h3>'
+        : (clase.raza ?
+            '<h3 class="clase-subtitulo clase-subtitulo_raza">Clase de raza</h3>' 
+        : "")))
+    end
 end
