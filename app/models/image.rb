5be98f486dedbc97cc107a13207b5082322bb416
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2025-09-28 14:22:24 +0200

    Image.content_type

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,4 +3,10 @@
 class Image < ApplicationRecord
   # atributes: nombre, data
   include ImageUrlable
+
+  def content_type
+    ext = File.extname(nombre).delete(".").downcase
+    mime_type = MIME::Types.type_for(ext).first
+    mime_type ? mime_type.content_type : "application/octet-stream"
+  end
 end
