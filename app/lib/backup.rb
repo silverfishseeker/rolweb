5255f4c52750a6b3e754d3415c1cca2ff327fb9e
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2025-09-29 17:13:45 +0200

    logs imagen a imagen en backup.restore

--- app/lib/backup.rb
+++ app/lib/backup.rb
@@ -130,10 +130,12 @@ module Backup
     imgdir = images_dir(restore_dir)
     temp_path = imgdir.join("uploading_image")
     imgdir.children.each do |model_dir|
+      Rails.logger.info "  Model_dir: #{model_dir}"
       model = model_dir.basename.to_s.safe_constantize
       metadata = JSON.parse(File.read(model_dir.join("images_meta.json")))
       SilverImageUploader.warn_on_remove_missing = false
       metadata.each do |entry|
+        Rails.logger.info "    Entry: id(#{entry["id"]}) filename(#{entry["original_filename"]})"
         id = entry["id"]
         
         file_path = model_dir.join(id.to_s)
