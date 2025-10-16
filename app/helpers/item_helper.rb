dd037139a91834e96b332cd758038d58fad24d4e
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2025-10-16 19:13:05 +0200

    ajustes sobre rituales

--- app/helpers/item_helper.rb
+++ app/helpers/item_helper.rb
@@ -1,32 +1,26 @@
 module ItemHelper
-  def ritual_description(item)
-    if item.ritual.present?
-      propiedades = []
 
-      # Niveles
-      item.ritual.ritual_nivel_rel_rituals.each do |rel|
-          valor = "nivel#{rel.ritual_nivel.valor}"
-          valor += "x#{rel.cantidad}" if rel.cantidad > 1
-          propiedades << valor
-      end
-
-      # Clases
-      item.ritual.ritual_clase_rel_rituals.each do |rel|
-          valor = rel.ritual_clase.valor
-          valor += "x#{rel.cantidad}" if rel.cantidad > 1
-          propiedades << valor
-      end
-
-      # Costes
-      item.ritual.ritual_coste_rel_rituals.each do |rel|
-          valor = rel.ritual_coste.valor
-          valor += "x#{rel.cantidad}" if rel.cantidad > 1
-          propiedades << valor
-      end
-        
-      "{#{propiedades.join(', ')}} "
-    else
-      ""
+  def sub_rd(src, propiedades)
+    src.each do |rel|
+      valor = yield rel
+      valor = "#{rel.cantidad}X" + valor if rel.cantidad > 1
+      propiedades << valor
     end
+  end
+
+  def ritual_description(item)
+    return "" unless item.ritual.present?
+
+    propiedades = []
+    sub_rd(item.ritual.ritual_nivel_rel_rituals, propiedades) do |rel|
+      "nivel#{rel.ritual_nivel.valor}"
+    end
+    sub_rd(item.ritual.ritual_clase_rel_rituals, propiedades) do |rel|
+      rel.ritual_clase.valor
+    end
+    sub_rd(item.ritual.ritual_coste_rel_rituals, propiedades) do |rel|
+      rel.ritual_coste.valor
+    end
+    "{#{propiedades.join(', ')}} "
   end
 end
