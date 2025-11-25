32c606464c72a5260a35c62dcf99e9d70abb3d96
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2025-11-25 17:06:20 +0100

    añadir nombre a usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # atributes: email, encrypted_password, reset_password_token, reset_password_sent_at
-  #  remember_created_at, rango
+  # atributes:
+  #   email, nombre,
+  #   encrypted_password, reset_password_token, reset_password_sent_at
+  #   remember_created_at, rango
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
