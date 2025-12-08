9cda435b3e29024cd991f8791299cfc6efde91a9
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2025-12-08 18:26:58 +0100

    url en los correos

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -10,3 +10,5 @@ ActionMailer::Base.smtp_settings = {
   authentication: :login,
   enable_starttls_auto: true
 }
+  # This is for generting URLs within mailers. Needed for devise
+  ActionMailer::Base.default_url_options = { host: EnvVars["APP_HOST"], port: 80 }
