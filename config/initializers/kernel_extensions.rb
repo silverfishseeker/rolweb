d74ca529adcb5e295ae59e6d4f0c3010a005a32d
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2025-12-11 14:13:08 +0100

    avances en el form de personaje

--- config/initializers/kernel_extensions.rb
+++ config/initializers/kernel_extensions.rb
@@ -1,12 +1,10 @@
 module Kernel
-  def error_coalesce(*allowed_exceptions)
-    begin
-      yield || nil
-    rescue NoMethodError 
-      nil
-    rescue => e
-      raise e unless allowed_exceptions.include?(e.class)
-      nil
-    end
+  def error_coalesce(*allowed_exceptions, default:nil)
+    yield || default
+  rescue NoMethodError 
+    default
+  rescue => e
+    raise e unless allowed_exceptions.include?(e.class)
+    default
   end
 end
