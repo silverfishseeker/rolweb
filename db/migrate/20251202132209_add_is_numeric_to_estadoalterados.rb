1bc7b259f974b0c628be8401db54de82f05d7c6c
silver <silverfishseeker@gmail.com>
2026-03-05 15:35:44 +0100

    fix en la migración

--- db/migrate/20251202132209_add_is_numeric_to_estadoalterados.rb
+++ db/migrate/20251202132209_add_is_numeric_to_estadoalterados.rb
@@ -1,4 +1,4 @@
-class AddIsNumericToEstadoalterado < ActiveRecord::Migration[8.0]
+class AddIsNumericToEstadoalterados < ActiveRecord::Migration[8.0]
   def change
     add_column :estadoalterados, :isNumeric, :boolean
   end
