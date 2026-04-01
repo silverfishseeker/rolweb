6278155e3a1c2afb1e2a255131f23fb88153345d
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2026-04-01 17:31:21 +0200

    Ver habilidades de clase en form personaje

--- app/models/habilidad.rb
+++ app/models/habilidad.rb
@@ -1,14 +1,29 @@
 class Habilidad < ApplicationRecord
-    # atributes: nombre, nivel, efecto, oculto
-    has_rich_text :efecto
+  # atributes: nombre, nivel, efecto, oculto, tipo
+  has_rich_text :efecto
 
-    mount_image_uploader
-    remove_attribute_if_checked :image
+  mount_image_uploader
+  remove_attribute_if_checked :image
 
-    has_and_belongs_to_many :clases
-    has_and_belongs_to_many :items
-    has_and_belongs_to_many :categs
-    has_and_belongs_to_many :mobs
+  has_and_belongs_to_many :clases
+  has_and_belongs_to_many :items
+  has_and_belongs_to_many :categs
+  has_and_belongs_to_many :mobs
 
-    scope :hide, ->(secreto=false) { where(oculto: secreto) }
+  scope :hide, ->(secreto=false) { where(oculto: secreto) }
+
+  def build_personaje_has_habilidad(personaje)
+    Pj::PersonajeHasHabilidad.new(habilidad: self, hasHabilidads: personaje, sobreescritura: nil)
+  end
+
+  def tipoImagen
+    ["red-triangle.png", 
+     "blue-circle.png", 
+     "orange-diamond.png",
+     "purple-triangle.png",
+     "green-square.png",
+     "yellow-triangle.png",
+     "white-square.png"
+    ][tipo]
+  end
 end
