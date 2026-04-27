4710d91ee5d78cf1b695b760b164a8d87513b6b6
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2026-04-27 14:13:22 +0200

    Arreglar entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e # Devolver error inmediatamente si un comando falla
 
-echo "EP: Preparando base de datos"
+echo "Entrypoint: Preparando base de datos"
 bundle exec rails db:prepare
 
-echo "EP: impiando tmp"
-bundle exec rails tmp:clear
+echo "Entrypoint: limpiando tmp"
+rm -rf /app/tmp/*
 
-echo "EP: Arrancando Rails"
+echo "Entrypoint: Arrancando Rails"
 exec bundle exec rails server -b 0.0.0.0 -e ${RAILS_ENV}
-
-echo "EP: Terminando entrypoint"
