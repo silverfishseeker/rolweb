30b0e2c043e6424b9c03f0a4c22976535cd29cb4
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2026-05-07 22:10:07 +0200

    fix again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 require "active_support/core_ext/integer/time"
-require "00_env_vars"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
