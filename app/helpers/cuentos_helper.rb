2a94f786a142089014ff671ec0c89194385541e4
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2026-07-25 13:07:58 +0200

    cuentos ocultos se añaden como hijos pero no se muestran

--- app/helpers/cuentos_helper.rb
+++ app/helpers/cuentos_helper.rb
@@ -4,25 +4,27 @@ module CuentosHelper
   SPECIAL_CHARACTER = "_"
 
   def calculate_cuento(cuento, do_childs = true)
-    cuentos = Cuento.where.not(id: cuento.id).where(oculto: false)
-        .select(:id, :nombre).map do |c|
+    cuentos = Cuento.where.not(id: cuento.id).map do |c|
       [c.nombre.downcase, c]
     end.to_h
+
+    html = cuento.texto.body.to_html
+
+    return html if !cuentos.any?
 
     regex = /\b(#{cuentos.keys.map { |n| Regexp.escape(n) }.join("|")})\b/ix
     
     cuento.childs.clear if do_childs
 
-    html = cuento.texto.body.to_html.gsub(regex) do |match|
+    html = html.gsub(regex) do |match|
       curr_cuento = cuentos[match.downcase]
       cuento.childs << curr_cuento if do_childs && !cuento.childs.include?(curr_cuento)
       match = match[1..-1] if match.starts_with?(SPECIAL_CHARACTER)
-      %(<a href="/cuentos/#{curr_cuento.id}">#{match}</a>)
+      curr_cuento.oculto ? match : %(<a href="/cuentos/#{curr_cuento.id}">#{match}</a>)
     end.html_safe
 
     cuento.save! if do_childs && cuento.changed?
     cache_set "cuentos_html_#{cuento.id}", html
-    
     html
   end
 
