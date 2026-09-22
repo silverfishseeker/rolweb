77bab1af5ff3de364ab831a4f1946e6d617a9d17
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2026-09-22 15:19:48 +0200

    fix mapeos estados de salud partes cuerpos

--- app/models/pj/parte_cuerpo.rb
+++ app/models/pj/parte_cuerpo.rb
@@ -16,11 +16,15 @@ class Pj::ParteCuerpo < ApplicationRecord
   DEFAULT_UNKNOWN_STATE = "?"
 
   DEFAULT_MAPEOS = {
-    1 => ["X", "S"],
-    2 => ["X", "G", "S"],
-    3 => ["X", "G", "H", "S"],
-    4 => ["X", "G", "H", "S", "S+"],
-    5 => ["X", "G", "H", "H", "S", "S+"]
+    1 => ["💀", "S"],
+    2 => ["💀", "G", "S"],
+    3 => ["💀", "G", "H", "S"],
+    4 => ["💀", "G", "H", "S", "S"],
+    5 => ["💀", "G", "H", "H", "S", "S"],
+    6 => ["💀", "G", "G", "H", "H", "S", "S"],
+    7 => ["💀", "G", "G", "H", "H", "S", "S", "S"],
+    8 => ["💀", "G", "G", "H", "H", "H", "S", "S", "S"],
+    9 => ["💀", "G", "G", "G", "H", "H", "H", "S", "S", "S"],
   }
 
   DEFAULT_MAPEO_NAME = "DEFAULT_MAPEO"
@@ -58,7 +62,7 @@ class Pj::ParteCuerpo < ApplicationRecord
         if mapeo == DEFAULT_MAPEO_NAME && DEFAULT_MAPEOS[saludmax]
           DEFAULT_MAPEOS[saludmax]
         else
-          str_mapeo_to_list(mapeo)
+          ["💀"] + str_mapeo_to_list(mapeo)
         end
       )[saludact] || DEFAULT_UNKNOWN_STATE
     end
